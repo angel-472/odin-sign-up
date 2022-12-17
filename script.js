316caa932458a78f73edccb0d5e95b6c9ace16e9
angel <108492882+angel-472@users.noreply.github.com>
2022-12-17 16:22:01 -0400

    Write some of the code

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const btnFirstName = document.getElementById('first-name');
+const btnLastName = document.getElementById('last-name');
+const btnEmail = document.getElementById('email');
+const btnFirstName = document.getElementById('first-name');
